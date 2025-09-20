b716dc335cc0618c828ee1a1bd3f241ac0ed52b3
hashimoto ryo <qualiamusic@outlook.jp>
2025-09-20 21:53:44 +0900

    Initial Commit

---
+++ kokokita/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kokokita
+//
+//  Created by 橋本遼 on 2025/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kokokita/kokokitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  kokokitaApp.swift
+//  kokokita
+//
+//  Created by 橋本遼 on 2025/09/20.
+//
+
+import SwiftUI
+
+@main
+struct kokokitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
